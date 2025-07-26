29dc3e3e11670c361dc8c27de64572148dad2727
JackChmel <jack.chmel@gmail.com>
2025-07-26 11:07:56 +0200

    Add files via upload

---
+++ wolfchunkloader_mod_project/src/main/java/com/example/wolfchunkloader/WolfChunkLoader.java
@@ -0,0 +1,10 @@
+package com.example.wolfchunkloader;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("wolfchunkloader")
+public class WolfChunkLoader {
+    public WolfChunkLoader() {
+        System.out.println("WolfChunkLoader mod initialized!");
+    }
+}
